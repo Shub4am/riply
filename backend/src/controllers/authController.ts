b40fb66f378b8e1ec2b9d41cd13a8723d2716e40
Shub4am <shub4am@gmail.com>
2025-07-08 19:48:00 +0530

    Update auth controller

--- backend/src/controllers/authController.ts
+++ backend/src/controllers/authController.ts
@@ -1,53 +1,87 @@
-import { Request, Response } from "express";
+import type { Request, Response } from "express";
 import bcrypt from "bcrypt";
-import { db } from "../config/db";
-import { users } from "../db/schema";
+import { db } from "../config/db.ts";
+import { users } from "../db/schema.ts";
 import { eq } from "drizzle-orm";
-import { signToken } from "../config/jwt";
+import { signToken } from "../config/jwt.ts";
 import { v4 as uuidv4 } from "uuid";
 
 export const signup = async (req: Request, res: Response) => {
-  const { name, email, password } = req.body;
+  try {
+    const { name, email, password } = req.body;
 
-  // check if user exists
-  const existingUser = await db
-    .select()
-    .from(users)
-    .where(eq(users.email, email));
-  if (existingUser.length > 0) {
-    return res.status(400).json({ error: "User already exists" });
+    if (!name || !email || !password) {
+      res.status(400).json({ error: "All fields are required." });
+      return;
+    }
+    if (password.length < 6) {
+      res
+        .status(400)
+        .json({ message: "Password should be at least 6 characters long" });
+      return;
+    }
+
+    if (name.length < 3) {
+      res
+        .status(400)
+        .json({ message: "Username should be at least 3 characters long" });
+      return;
+    }
+
+    const existingUser = await db
+      .select()
+      .from(users)
+      .where(eq(users.email, email));
+
+    if (existingUser.length > 0) {
+      res.status(400).json({ error: "User already exists" });
+      return;
+    }
+
+    const hashed = await bcrypt.hash(password, 10);
+    const userId = uuidv4();
+
+    await db.insert(users).values({
+      id: userId,
+      name,
+      email,
+      passwordHash: hashed,
+    });
+
+    const token = signToken({ id: userId, email });
+    res.status(201).json({ token });
+  } catch (error) {
+    console.error("Signup error:", error);
+    res.status(500).json({ error: "Something went wrong" });
   }
-
-  const hashed = await bcrypt.hash(password, 10);
-  const userId = uuidv4();
-
-  await db.insert(users).values({
-    id: userId,
-    name,
-    email,
-    passwordHash: hashed,
-  });
-
-  const token = signToken({ id: userId, email });
-  res.status(201).json({ token });
 };
 
 export const login = async (req: Request, res: Response) => {
-  const { email, password } = req.body;
+  try {
+    const { email, password } = req.body;
 
-  const existing = await db.select().from(users).where(eq(users.email, email));
+    const existing = await db
+      .select()
+      .from(users)
+      .where(eq(users.email, email));
 
-  if (existing.length === 0) {
-    return res.status(400).json({ error: "Invalid credentials" });
+    if (existing.length === 0) {
+      res.status(400).json({ error: "Invalid credentials" });
+      return;
+    }
+
+    const user = existing[0];
+    const match = await bcrypt.compare(password, user.passwordHash);
+    if (!match) {
+      res.status(400).json({ error: "Invalid credentials" });
+      return;
+    }
+
+    const token = signToken({ id: user.id, email });
+
+    res.status(200).json({ token });
+  } catch (error) {
+    console.log("Error in login route", error);
+    res.status(500).json({ message: "Internal server error" });
   }
-
-  const user = existing[0];
-  const match = await bcrypt.compare(password, user.passwordHash);
-  if (!match) {
-    return res.status(400).json({ error: "Invalid credentials" });
-  }
-
-  const token = signToken({ id: user.id, email });
-
-  res.status(200).json({ token });
 };
