11ab1c0def6208c5900591d21dc7966263bda742
Shub4am <shub4am@gmail.com>
2025-07-09 00:11:53 +0530

    Update challenge routes

---
+++ backend/src/routes/challengeRoutes.ts
@@ -0,0 +1,19 @@
+import express from "express";
+import {
+  createChallenge,
+  getAllChallenges,
+  joinChallenge,
+  leaveChallenge,
+  getUserChallenges,
+} from "../controllers/challengeController.ts";
+import { requireAuth } from "../middleware/auth.middleware.ts";
+
+const router = express.Router();
+
+router.post("/", requireAuth, createChallenge);
+router.get("/", getAllChallenges);
+router.post("/:id/join", requireAuth, joinChallenge);
+router.delete("/:id/leave", requireAuth, leaveChallenge);
+router.get("/mine", requireAuth, getUserChallenges);
+
+export default router;
