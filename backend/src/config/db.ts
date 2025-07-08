3b37c98bf36fced315f263cfb2ec61d529978ca4
Shub4am <shub4am@gmail.com>
2025-07-08 19:48:27 +0530

    Update db

--- backend/src/config/db.ts
+++ backend/src/config/db.ts
@@ -1,12 +1,9 @@
-import { drizzle } from "drizzle-orm/node-postgres";
-import { Pool } from "pg";
-import * as schema from "../db/schema";
+import { neon } from "@neondatabase/serverless";
+import { drizzle } from "drizzle-orm/neon-http";
+import * as schema from "../db/schema.ts";
 import dotenv from "dotenv";
 
 dotenv.config();
 
-const pool = new Pool({
-  connectionString: process.env.DATABASE_URL,
-});
-
-export const db = drizzle(pool, { schema });
+const sql = neon(process.env.DATABASE_URL!);
+export const db = drizzle(sql, { schema });
