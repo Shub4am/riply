57466c8aaad626997865b11e6886a03491761b48
Shub4am <shub4am@gmail.com>
2025-07-09 00:13:15 +0530

    Update challenges route in server.js

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -2,6 +2,7 @@ import express from "express";
 import dotenv from "dotenv";
 import cors from "cors";
 import authRoutes from "./routes/authRoutes.ts";
+import challengeRoutes from "./routes/challengeRoutes.ts";
 
 dotenv.config();
 const app = express();
@@ -11,6 +12,7 @@ app.use(express.json());
 
 // Routes
 app.use("/api/auth", authRoutes);
+app.use("/api/challenges", challengeRoutes);
 
 app.get("/", (_, res) => {
   res.status(200).send("Riply API running");
