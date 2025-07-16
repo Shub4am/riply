f5381a66e16e4c22c73e0404022def64c99bdcdf
Shub4am <shub4am@gmail.com>
2025-07-16 20:37:51 +0530

    Init zustand authStore and signup async function

---
+++ mobile/store/authStore.ts
@@ -0,0 +1,61 @@
+import { create } from "zustand";
+import AsyncStorage from "@react-native-async-storage/async-storage";
+
+type User = {
+  id: string;
+  name: string;
+  email: string;
+};
+
+type AuthStoreState = {
+  user: User | null;
+  token: string | null;
+  isLoading: boolean;
+  signup: (
+    name: string,
+    email: string,
+    password: string
+  ) => Promise<{ success: boolean; error?: string }>;
+};
+
+export const useAuthStore = create<AuthStoreState>((set) => ({
+  user: null,
+  token: null,
+  isLoading: false,
+
+  signup: async (name, email, password) => {
+    set({ isLoading: true });
+    try {
+      const response = await fetch("http://localhost:5001/api/auth/signup", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          name,
+          email,
+          password,
+        }),
+      });
+
+      const data = await response.json();
+
+      if (!response.ok)
+        throw new Error(data.message || "Something went wrong!");
+
+      await AsyncStorage.setItem("user", JSON.stringify(data.user));
+      await AsyncStorage.setItem("token", data.token);
+
+      set({ token: data.token, user: data.user, isLoading: false });
+
+      return { success: true };
+    } catch (error) {
+      set({ isLoading: false });
+      if (error instanceof Error) {
+        return { success: false, error: error.message };
+      }
+
+      return { success: false, error: "An unknown error occurred" };
+    }
+  },
+}));
