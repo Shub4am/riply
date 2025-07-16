db498066d79049cac98bca2cbcf5e84789e7b8d9
Shub4am <shub4am@gmail.com>
2025-07-16 16:51:57 +0530

    Update local fonts

--- mobile/app/_layout.tsx
+++ mobile/app/_layout.tsx
@@ -1,8 +1,28 @@
 import SafeScreen from "@/components/SafeScreen";
 import { Stack } from "expo-router";
+import * as SplashScreen from "expo-splash-screen";
+import { useFonts } from "expo-font";
 import { SafeAreaProvider } from "react-native-safe-area-context";
+import { useEffect } from "react";
+
+SplashScreen.preventAutoHideAsync();
 
 export default function RootLayout() {
+  const [loaded, error] = useFonts({
+    "Poppins-Regular": require("../assets/fonts/Poppins-Regular.ttf"),
+    "Poppins-Medium": require("../assets/fonts/Poppins-Medium.ttf"),
+    "Poppins-Bold": require("../assets/fonts/Poppins-Bold.ttf"),
+  });
+
+  useEffect(() => {
+    if (loaded || error) {
+      SplashScreen.hideAsync();
+    }
+  }, [loaded, error]);
+
+  if (!loaded && !error) {
+    return null;
+  }
   return (
     <SafeAreaProvider>
       <SafeScreen>
