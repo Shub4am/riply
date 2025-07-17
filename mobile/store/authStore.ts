9b68c521482ccf323b6a2cf364b4c5864ae56068
Shub4am <shub4am@gmail.com>
2025-07-17 06:04:35 +0530

    Update login, logout and checkAuth async fn

--- mobile/store/authStore.ts
+++ mobile/store/authStore.ts
@@ -17,6 +17,12 @@ type AuthStoreState = {
     email: string,
     password: string
   ) => Promise<{ success: boolean; error?: string }>;
+  login: (
+    email: string,
+    password: string
+  ) => Promise<{ success: boolean; error?: string }>;
+  checkAuth: () => Promise<void>;
+  logout: () => Promise<void>;
 };
 
 export const useAuthStore = create<AuthStoreState>((set) => ({
@@ -41,8 +47,7 @@ export const useAuthStore = create<AuthStoreState>((set) => ({
 
       const data = await response.json();
 
-      if (!response.ok)
-        throw new Error(data.message || "Something went wrong!");
+      if (!response.ok) throw new Error(data.message || "Something went wrong");
 
       await AsyncStorage.setItem("user", JSON.stringify(data.user));
       await AsyncStorage.setItem("token", data.token);
@@ -57,5 +62,55 @@ export const useAuthStore = create<AuthStoreState>((set) => ({
 
       return { success: false, error: "An unknown error occurred" };
     }
+  },
+
+  checkAuth: async () => {
+    try {
+      const token = await AsyncStorage.getItem("token");
+      const userJson = await AsyncStorage.getItem("user");
+      const user = userJson ? JSON.parse(userJson) : null;
+
+      set({ token, user });
+    } catch (error) {
+      console.log("Auth check failed", error);
+    }
+  },
+
+  login: async (email, password) => {
+    set({ isLoading: true });
+    try {
+      const response = await fetch(`${API_URL}/api/auth/login`, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          email,
+          password,
+        }),
+      });
+      const data = await response.json();
+
+      if (!response.ok) throw new Error(data.message || "Something went wrong");
+
+      await AsyncStorage.setItem("user", JSON.stringify(data.user));
+      await AsyncStorage.setItem("token", data.token);
+
+      set({ token: data.token, user: data.user, isLoading: false });
+      return { success: true };
+    } catch (error) {
+      set({ isLoading: false });
+      if (error instanceof Error) {
+        return { success: false, error: error.message };
+      }
+
+      return { success: false, error: "An unknown error occurred" };
+    }
+  },
+
+  logout: async () => {
+    await AsyncStorage.removeItem("token");
+    await AsyncStorage.removeItem("user");
+    set({ token: null, user: null });
   },
 }));
