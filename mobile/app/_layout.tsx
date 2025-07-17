a3ba076f73073f46e3e4ed150501779e67a8e140
Shub4am <shub4am@gmail.com>
2025-07-17 07:04:17 +0530

    Update layout with tabs and auth screens

--- mobile/app/_layout.tsx
+++ mobile/app/_layout.tsx
@@ -1,9 +1,10 @@
 import SafeScreen from "@/components/SafeScreen";
-import { Stack } from "expo-router";
+import { Stack, useRouter, useSegments } from "expo-router";
 import * as SplashScreen from "expo-splash-screen";
 import { useFonts } from "expo-font";
 import { SafeAreaProvider } from "react-native-safe-area-context";
 import { useEffect } from "react";
+import { useAuthStore } from "@/store/authStore";
 
 SplashScreen.preventAutoHideAsync();
 
@@ -23,11 +24,30 @@ export default function RootLayout() {
   if (!loaded && !error) {
     return null;
   }
+
+  const router = useRouter();
+  const segments = useSegments();
+
+  const { checkAuth, user, token } = useAuthStore();
+
+  useEffect(() => {
+    checkAuth();
+  }, []);
+
+  // handle navigation based on the auth state
+  useEffect(() => {
+    const inAuthScreen = segments[0] === "(auth)";
+    const isSignedIn = user && token;
+
+    if (!isSignedIn && !inAuthScreen) router.replace("/(auth)/login");
+    else if (isSignedIn && inAuthScreen) router.replace("/(tabs)/home");
+  }, [user, token, segments]);
+
   return (
     <SafeAreaProvider>
       <SafeScreen>
         <Stack screenOptions={{ headerShown: false, statusBarStyle: "dark" }}>
-          <Stack.Screen name="index" />
+          <Stack.Screen name="(tabs)" />
           <Stack.Screen name="(auth)" />
         </Stack>
       </SafeScreen>
