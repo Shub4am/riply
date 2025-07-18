2b31b381a3b253cf3525463a34d1058741f2b58b
Shub4am <shub4am@gmail.com>
2025-07-18 15:14:15 +0530

    Update root layout rendering issue

--- mobile/app/_layout.tsx
+++ mobile/app/_layout.tsx
@@ -1,5 +1,10 @@
 import SafeScreen from "@/components/SafeScreen";
-import { Stack, useRouter, useSegments } from "expo-router";
+import {
+  Stack,
+  useRouter,
+  useSegments,
+  useRootNavigationState,
+} from "expo-router";
 import * as SplashScreen from "expo-splash-screen";
 import { useFonts } from "expo-font";
 import { SafeAreaProvider } from "react-native-safe-area-context";
@@ -9,39 +14,44 @@ import { useAuthStore } from "@/store/authStore";
 SplashScreen.preventAutoHideAsync();
 
 export default function RootLayout() {
-  const [loaded, error] = useFonts({
+  const router = useRouter();
+  const segments = useSegments();
+  const navigationState = useRootNavigationState();
+
+  const { checkAuth, user, token, authChecked } = useAuthStore();
+
+  const [fontsLoaded] = useFonts({
     "Poppins-Regular": require("../assets/fonts/Poppins-Regular.ttf"),
     "Poppins-Medium": require("../assets/fonts/Poppins-Medium.ttf"),
     "Poppins-Bold": require("../assets/fonts/Poppins-Bold.ttf"),
   });
 
   useEffect(() => {
-    if (loaded || error) {
-      SplashScreen.hideAsync();
-    }
-  }, [loaded, error]);
-
-  if (!loaded && !error) {
-    return null;
-  }
-
-  const router = useRouter();
-  const segments = useSegments();
-
-  const { checkAuth, user, token } = useAuthStore();
-
-  useEffect(() => {
     checkAuth();
   }, []);
 
-  // handle navigation based on the auth state
   useEffect(() => {
-    const inAuthScreen = segments[0] === "(auth)";
-    const isSignedIn = user && token;
+    if (fontsLoaded && authChecked) {
+      SplashScreen.hideAsync();
+    }
+  }, [fontsLoaded, authChecked]);
 
-    if (!isSignedIn && !inAuthScreen) router.replace("/(auth)/login");
-    else if (isSignedIn && inAuthScreen) router.replace("/(tabs)/home");
-  }, [user, token, segments]);
+  useEffect(() => {
+    if (!navigationState?.key || !authChecked) return;
+
+    const inAuthScreen = segments[0] === "(auth)";
+    const isSignedIn = !!user && !!token;
+
+    if (!isSignedIn && !inAuthScreen) {
+      router.replace("/(auth)/login");
+    } else if (isSignedIn && inAuthScreen) {
+      router.replace("/(tabs)/home");
+    }
+  }, [user, token, segments, authChecked, navigationState]);
+
+  if (!fontsLoaded || !authChecked || !navigationState?.key) {
+    return null;
+  }
 
   return (
     <SafeAreaProvider>
