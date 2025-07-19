64d9e8fafbcaab6b7e08a0f0ed023040f0b2a2c6
Shub4am <shub4am@gmail.com>
2025-07-20 03:13:05 +0530

    Init create tab styles

---
+++ mobile/assets/styles/createTab.styles.ts
@@ -0,0 +1,145 @@
+import { StyleSheet } from "react-native";
+import COLORS from "@/constants/colors";
+
+const styles = StyleSheet.create({
+  KeyboardAvoidingViewStyle: {
+    flex: 1,
+  },
+  scrollViewStyle: {
+    flex: 1,
+    backgroundColor: COLORS.background,
+  },
+  container: {
+    flexGrow: 1,
+    justifyContent: "center",
+    padding: 20,
+    backgroundColor: COLORS.background,
+  },
+  card: {
+    backgroundColor: COLORS.cardBackground,
+    borderRadius: 16,
+    padding: 20,
+    marginVertical: 16,
+    shadowColor: COLORS.black,
+    shadowOffset: { width: 0, height: 2 },
+    shadowOpacity: 0.1,
+    shadowRadius: 8,
+    elevation: 3,
+    borderWidth: 1,
+    borderColor: COLORS.border,
+  },
+
+  header: {
+    alignItems: "center",
+    marginBottom: 24,
+  },
+  title: {
+    fontSize: 24,
+    fontFamily: "Poppins-Bold",
+    includeFontPadding: false,
+    color: COLORS.textPrimary,
+    marginBottom: 8,
+  },
+  subtitle: {
+    fontSize: 14,
+    fontFamily: "Poppins-Regular",
+    includeFontPadding: false,
+    color: COLORS.textSecondary,
+    textAlign: "center",
+  },
+
+  form: {
+    marginBottom: 16,
+  },
+  formGroup: {
+    marginBottom: 20,
+  },
+  label: {
+    fontSize: 14,
+    fontFamily: "Poppins-Regular",
+    includeFontPadding: false,
+    marginBottom: 8,
+    color: COLORS.textPrimary,
+    fontWeight: "500",
+  },
+  inputContainer: {
+    flexDirection: "row",
+    alignItems: "center",
+    backgroundColor: COLORS.inputBackground,
+    borderRadius: 12,
+    borderWidth: 1,
+    borderColor: COLORS.border,
+    paddingHorizontal: 12,
+  },
+  inputIcon: {
+    marginRight: 10,
+  },
+  input: {
+    flex: 1,
+    height: 48,
+    color: COLORS.textDark,
+    fontFamily: "Poppins-Regular",
+    includeFontPadding: false,
+  },
+  textArea: {
+    backgroundColor: COLORS.inputBackground,
+    borderRadius: 12,
+    borderWidth: 1,
+    borderColor: COLORS.border,
+    padding: 12,
+    height: 100,
+    color: COLORS.textDark,
+    fontFamily: "Poppins-Regular",
+    includeFontPadding: false,
+  },
+  imagePicker: {
+    width: "100%",
+    height: 200,
+    backgroundColor: COLORS.inputBackground,
+    borderRadius: 12,
+    borderWidth: 1,
+    borderColor: COLORS.border,
+    overflow: "hidden",
+  },
+  previewImage: {
+    width: "100%",
+    height: "100%",
+  },
+  placeholderContainer: {
+    width: "100%",
+    height: "100%",
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  placeholderText: {
+    color: COLORS.textSecondary,
+    fontFamily: "Poppins-Regular",
+    includeFontPadding: false,
+    marginTop: 8,
+  },
+  button: {
+    backgroundColor: COLORS.primary,
+    borderRadius: 12,
+    height: 50,
+    flexDirection: "row",
+    justifyContent: "center",
+    alignItems: "center",
+    marginTop: 16,
+    shadowColor: COLORS.black,
+    shadowOffset: { width: 0, height: 2 },
+    shadowOpacity: 0.1,
+    shadowRadius: 4,
+    elevation: 2,
+  },
+  buttonText: {
+    color: COLORS.white,
+    fontSize: 18,
+    fontFamily: "Poppins-Medium",
+    includeFontPadding: false,
+  },
+  buttonIcon: {
+    marginRight: 8,
+  },
+});
+
+export default styles;
