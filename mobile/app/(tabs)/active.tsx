d8bc83a8ded4686c9759f1225315acc00c66eb8d
Shub4am <shub4am@gmail.com>
2025-07-26 06:06:15 +0530

    Update Active tab

--- mobile/app/(tabs)/active.tsx
+++ mobile/app/(tabs)/active.tsx
@@ -1,10 +1,56 @@
-import { View, Text } from "react-native";
-import React from "react";
+import { View, Text, Alert } from "react-native";
+import React, { useEffect, useState } from "react";
+import { useRouter } from "expo-router";
+import { API_URL } from "@/constants/api";
+import { useAuthStore } from "@/store/authStore";
+import styles from "@/assets/styles/activeTab.styles";
+import ProfileHeader from "@/components/ProfileHeader";
+import LogoutBtn from "@/components/LogoutBtn";
 
 export default function Active() {
+  const [challenges, setChallenges] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+  const [isRefreshing, setIsRefreshing] = useState(false);
+
+  const router = useRouter();
+  const { token } = useAuthStore();
+
+  const fetchUserChallenges = async () => {
+    try {
+      setIsLoading(true);
+
+      const response = await fetch(`${API_URL}/api/challenges/mine`, {
+        headers: { Authorization: `Bearer: ${token}` },
+      });
+      const data = await response.json();
+      if (!response.ok)
+        throw new Error(data.message || "Failed to fetch active challenges");
+
+      setChallenges(data);
+
+      //
+    } catch (error) {
+      console.error("Error fetching challenges: ", error);
+      Alert.alert("Error", "Failed to load active challenges.");
+    } finally {
+      setIsLoading(false);
+    }
+  };
+
+  useEffect(() => {
+    fetchUserChallenges();
+  }, []);
+
   return (
-    <View>
-      <Text>Active tab</Text>
+    <View style={styles.container}>
+      <ProfileHeader />
+      <LogoutBtn />
+      <View style={styles.challengesHeader}>
+        <Text style={styles.challengeTitle}>Your active challenges</Text>
+        <Text style={styles.challengesCount}>
+          {challenges.length} challenges
+        </Text>
+      </View>
     </View>
   );
 }
