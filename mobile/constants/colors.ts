15e2266a7b02ccbf7cd6cf686b7dbdba34467ead
Shub4am <shub4am@gmail.com>
2025-07-28 06:10:40 +0530

    Update dark colors

--- mobile/constants/colors.ts
+++ mobile/constants/colors.ts
@@ -1,17 +1,32 @@
-// FOREST
+// FOREST Light
+// const COLORS = {
+//   primary: "#4CAF50",
+//   textPrimary: "#2e5a2e",
+//   textSecondary: "#688f68",
+//   textDark: "#1b361b",
+//   placeholderText: "#767676",
+//   background: "#e8f5e9",
+//   cardBackground: "#f1f8f2",
+//   inputBackground: "#f4faf5",
+//   border: "#c8e6c9",
+//   white: "#ffffff",
+//   black: "#000000",
+//   red: "#b32f2f",
+// };
+// Forest Dark
 const COLORS = {
-  primary: "#4CAF50",
-  textPrimary: "#2e5a2e",
-  textSecondary: "#688f68",
-  textDark: "#1b361b",
-  placeholderText: "#767676",
-  background: "#e8f5e9",
-  cardBackground: "#f1f8f2",
-  inputBackground: "#f4faf5",
-  border: "#c8e6c9",
+  primary: "#31C950",
+  textPrimary: "#E7E5E4",
+  textSecondary: "#A6A09B",
+  textDark: "#D4D4D8",
+  placeholderText: "#E4E4E7",
+  background: "#232228",
+  cardBackground: "#151419",
+  inputBackground: "#27272A",
+  border: "#32313b",
   white: "#ffffff",
   black: "#000000",
-  red: "#b32f2f",
+  red: "#b91826",
 };
 
 export default COLORS;
